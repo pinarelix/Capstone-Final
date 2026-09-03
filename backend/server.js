f449d4fef1fe17b2c39fa11da4a1753ca5fede4e
pinarelix <abellakevin12@gmail.com>
2026-09-03 17:24:25 +0800

    password error fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const Joi = require('joi');
 const path = require('path');
 const cartEngine = require('./cart-engine');
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, '.env') });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
